d17ae3ba668458016f5affb55b92bbfc6d7d0f0c
Don Prolad <donp@localhost.localdomain>
2017-09-19 11:17:47 +0800

    Array, Hashes, Bactracking

---
+++ my_boggle.rb
@@ -0,0 +1,14 @@
+# Boggle board
+num = 4
+board = ("A".."Z").to_a
+p board
+
+# board.map! {|row| row.map { |element| ("A".."Z").to_a.sample }}
+
+current_word = gets.chomp
+
+p current_word
+p current_word.chars
+def word_search(current_word, used_letters, current_position)
+
+end
